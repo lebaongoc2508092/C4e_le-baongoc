879fe2be41bd8b04983d7c997c358182e47ce16a
lebaongoc250892 <lebaongoc250892@gmail.com>
2021-07-22 20:13:30 +0700

    22-7

--- bai 12/main.js
+++ bai 12/main.js
@@ -14,17 +14,16 @@ let table = document.querySelector("#list-Students tbody");
 console.log(table)
 let strHTML="";
 for(let i = 0 ; i < listStudents.length; i++){
-    strHTML+=`<tr><td>${listStudents[i].id}</td><td>${listStudents[i].name}</td><td>${listStudents[i].age}</td><td>${listStudents[i].className}</td></tr>`
+    strHTML+=`<tr><td>${listStudents[i].id}</td><td>${listStudents[i].name}</td><td>${listStudents[i].age}</td><td>${listStudents[i].className}</td>
+    <td><button onclick="delete_studen(${listStudents[i].id})">xoa</button></td>
+    <td><button onclick="edit_studen(${listStudents[i].id})">sua</button></td></tr>`
 }
+
 console.log(strHTML)
 table.innerHTML=strHTML;
  }
 
-// function render(){
-//     for(let i = 0 ; i < listStudents.length; i++){
-//         strHTML+=`<tr><td>${listStudents[i].id}</td><td>${listStudents[i].name}</td><td>${listStudents[i].age}</td><td>${listStudents[i].className}</td></tr>`
-//     }
-// }
+
 function start(){
     render();
 }
@@ -40,6 +39,46 @@ function addNew(){
     listStudents.push(studen);
     console.log(listStudents)
     render();
+    
 }
 document.getElementById('btn-on').onclick = addNew;
 start();
+function delete_studen(id){
+    let index = listStudents.findIndex(function(item){
+        return item.id === id;
+    });
+    listStudents.splice(index,1)
+    render();
+}
+let  ip = document.querySelectorAll('input');
+let edit = 0;
+function edit_studen(id){
+    let index = listStudents.findIndex(function(item){
+        return item.id === id;
+    });
+    edit = index;
+    ip[0].value = listStudents[index].name;
+    ip[1].value = listStudents[index].age;
+    ip[2].value = listStudents[index].className;
+
+}
+document.getElementById('btn2-on').onclick=function(){
+    listStudents[edit].name=ip[0].value;
+    listStudents[edit].age=ip[1].value;
+    listStudents[edit].className=ip[2].value;
+    render();
+}
+let search = document.getElementById('search');
+function timkiem(){
+    let a = listStudents.find(function(item){
+        return listStudents = search
+        
+    }
+
+
+
+
+
+
+
+ 
