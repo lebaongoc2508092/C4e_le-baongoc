2f928b219c9a8e0a0e2785e79f199fafb09be19e
lebaongoc250892 <lebaongoc250892@gmail.com>
2021-07-24 22:44:57 +0700

    24-07

---
+++ w3_band/w3main.js
@@ -0,0 +1,19 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket');
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-modal-close')
+const modalContainer = document.querySelector('.js-modal-container')
+function showBuyTickets(){
+    modal.classList.add('open')
+}
+function hideBuyTickets(){
+    modal.classList.remove('open')
+}
+for(const buyBtn of buyBtns){
+    buyBtn.addEventListener('click',showBuyTickets)
+}
+modalClose.addEventListener('click',hideBuyTickets)
+modal.addEventListener('click',hideBuyTickets)
+modalContainer.addEventListener('click',function(event){
+    event.stopPropagation()
+})
+
