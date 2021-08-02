1fc293ff66628023b5a959fdbb4dfd0b53fc7679
lebaongoc250892 <lebaongoc250892@gmail.com>
2021-08-02 22:06:44 +0700

    responsive

--- w3_band/w3main.js
+++ w3_band/w3main.js
@@ -16,4 +16,29 @@ modal.addEventListener('click',hideBuyTickets)
 modalContainer.addEventListener('click',function(event){
     event.stopPropagation()
 })
-
+// đóng mở mobile - menu
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu')
+var headerHeight = header.clientHeight
+mobileMenu.onclick= function(){
+    var isClosed = header.clientHeight === headerHeight;
+    if(isClosed){
+        header.style.height ='auto';
+    }
+    else{
+        header.style.height = null;
+    }
+}
+// tự động đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+for(var i = 0 ; i< menuItems.length; i++){
+    var menuItem = menuItems[i]
+    menuItem.onclick = function(event){
+    var isParentMenu = this.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav')
+        if(isParentMenu){
+            event.preventDefault();
+    }else{ header.style.height = null;
+       
+    }
+}
+}
